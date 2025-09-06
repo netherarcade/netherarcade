dbcfd368fe46b362c35143de702403907990ec5f
Netherarcade <netherarcade@gmail.com>
2025-09-06 20:09:57 +0200

    Update register.js

--- login/register.js
+++ login/register.js
@@ -1,27 +1,2 @@
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC3RqOOLnHder2fx-Zofjob3I41Gp7apAg",
-    authDomain: "netherarcade-6dcd1.firebaseapp.com",
-    projectId: "netherarcade-6dcd1",
-    storageBucket: "netherarcade-6dcd1.firebasestorage.app",
-    messagingSenderId: "749509438797",
-    appId: "1:749509438797:web:f4d71f7c92ca178b321b04"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-//imputs
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-
-const submit = document.getElementById('submit').value;
-submit.addEventLister("click",function (event) {
-event.preventDefault()
-  alert(5)
-})
+ 
